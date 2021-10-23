d39dbf6265015dcafbb59e3d4ac917646e04fbec
Gurveer Parmar <gurveerparmar92@gmail.com>
2021-10-22 19:58:24 -0700

    using axios to get db

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import Person from './components/Person';
 import PersonForm from './components/PersonForm';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newSearch, setNewSearch] = useState('');
+
+  const hook = () => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then((res) => {
+      console.log('promise fulfilled');
+      setPersons(res.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -32,6 +38,7 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObject));
       setNewName('');
+      setNewNumber('');
     }
   };
 
